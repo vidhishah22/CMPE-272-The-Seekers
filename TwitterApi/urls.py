95e6bfd5c212b8a9fe0193aedd2aef6c7040bc53
“vidhishah22” <vidhirajesh.shah@sjsu.edu>
2018-10-10 10:28:58 -0700

    Adding idea files

--- TwitterApi/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include, path
-from . import views
-
-urlpatterns = [
-    #/TwitterApi/TheSeekers/
-    path('TheSeekers/', views.getusertweets, name='getusertweets'),
-    ]
