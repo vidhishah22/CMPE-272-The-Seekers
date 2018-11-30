d884f354a02123735c857ef39a7c89cc38436d79
pragyagautam <pragya.gautam.92@gmail.com>
2018-11-29 22:22:38 -0800

    html connection with django

--- LMSProject/LMS/urls.py
+++ LMSProject/LMS/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('TheSeekers/', views.index, name='index'),
-    path('TheSeekers/getprofile', views.profile, name='profile'),
+    path('TheSeekers/base', views.base, name='base'),
+    path('TheSeekers/Home', views.Home, name='Home'),
+    path('TheSeekers/profile', views.profile, name='profile'),
+    path('TheSeekers/ApplyForLeave', views.ApplyForLeave, name='ApplyForLeave'),
+    path('TheSeekers/ApproveLeave', views.ApproveLeave, name='ApproveLeave'),
+    path('TheSeekers/EditEmployee', views.EditEmployee, name='EditEmployee'),
 ]
--- LMSProject/LMS/views.py
+++ LMSProject/LMS/views.py
@@ -1,14 +1,48 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Employee
 from django.shortcuts import render
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+import json
+from LMS.models import Employee
+from django.http import JsonResponse
 
 
 def index(request):
     return HttpResponse("Hello, World")
 
+def base(request):
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
+    return render(request, 'LMS/base.html', {'employees': employee_list})
 
 def profile(request):
-
-    # Email_id = Employee.objects.filter(Email_Address__startswith='!????')
-    employee_list = Employee.objects.get(Emp_No=10001)
+    #user=request.user
+    #auth0user=user.social_auth.get(provider="auth0")
+    userdata = {
+        #'user_id': JsonResponse(Employee.objects.raw("select First_Name from Employee where Emp_No=1 "),safe=False),
+        #'user_id': user.id,
+        #'name': user.first_name,
+        #'picture': auth0user.extra_data['picture']
+    }
+    #Email_id=Employee.object.filter(Email_Address_startswith=????)
+    #print(auth0user.extra_data['email'])
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
     return render(request, 'LMS/profile.html', {'employees': employee_list})
+    #return render(request, 'index.html')
+
+def Home(request):
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
+    return render(request, 'LMS/Home.html', {'employees': employee_list})
+
+def ApplyForLeave(request):
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
+    return render(request, 'LMS/ApplyForLeave.html', {'employees': employee_list})
+
+def ApproveLeave(request):
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
+    return render(request, 'LMS/ApproveLeave.html', {'employees': employee_list})
+
+def EditEmployee(request):
+    employee_list = Employee.objects.get(Email_Address='pragya.gautam@sjsu.edu')
+    return render(request, 'LMS/EditEmployee.html', {'employees': employee_list})
