d0e84ba4e785ddbf663a4c1b2761c79e1217847b
“vidhishah22” <vidhirajesh.shah@sjsu.edu>
2018-12-07 12:51:06 -0800

    first test v1

--- LMSProject/LMS/tests.py
+++ LMSProject/LMS/tests.py
@@ -1,82 +1,72 @@
 from django.test import TestCase
 
 # Create your tests here.
-"""
-"""
-#@Author: 'Pragya Gautam'
-
-"""
 
 import unittest
 from django.test.client import RequestFactory
 from .views import *
-
-
+from django.contrib.sessions.middleware import SessionMiddleware
+from LMS.models import *
+from django.test import Client
+import jwt
+from unittest.mock import patch, Mock
 class SimpleTest(unittest.TestCase):
 
     #export DJANGO_SETTINGS_MODULE=TwitterApi.settings
+    emp=None
+
+    @classmethod
+    def setUpTestData(cls):
+        Employee.objects.create(First_Name='Big', Last_Name='Bob', Email_Address='bigbob@gmail.com', Emp_No=9999,
+                                Birth_Date='1953-09-02', Hire_Date='1986-06-26')
 
     def setUp(self):
         # Every test needs access to the request factory.
         self.factory = RequestFactory()
+        self.client = Client()
+        # Employee.objects.create(First_Name='Big', Last_Name='Bob', Email_Address='bigbob@gmail.com', Emp_No=9999, Birth_Date='1953-09-02', Hire_Date='1986-06-26')
 
-    def test_get_user_tweets(self):
-        print("TC1: Get User Tweets Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
+    def add_session_to_request(self, request):
+        """Annotate a request object with a session"""
+        middleware = SessionMiddleware()
+        middleware.process_request(request)
+        request.session.save()
+        return request
 
-        output1 = get_user_tweets(request)
-        self.assertEqual(output1.status_code, 200)
-
-    def test_get_mention_tweets(self):
-        print("TC2: Get Mention Tweets Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output2 = get_mention_tweets(request)
+    @patch('jwt.decode')
+    def test_profile(self, mock_jwt):
+        print("TC2: Get Profile Page:")
+        request = self.factory.get('/LMS/TheSeekers/')
+        request=self.add_session_to_request(request)
+        # print ('2222222---->', request.session)
+        # print(self.emp)
+        request.session['id_token']="abcd"
+        userinfo =  {'http://Lms.seekers.com/app_metadata': {'role': 'Manager'}, 'http://Lms.seekers.com/user_metadata': {}, 'given_name': 'Big', 'name': 'Big Bob', 'picture': 'https://lh4.googleusercontent.com/-rW-L8-Mdn6Y/AAAAAAAAAAI/AAAAAAAAGj0/dxNCMduKcmM/photo.jpg', 'gender': 'female', 'locale': 'en-GB', 'updated_at': '2018-12-07T18:18:05.806Z', 'email': 'bigbob@gmail.com', 'email_verified': True, 'iss': 'https://seekerslms.auth0.com/', 'sub': 'google-oauth2|104087801749009073568', 'aud': 'LCTMUEpEUe9eV_0NWzAkUvkqF6cC19aT', 'iat': 1544206686, 'exp': 1544242686}
+        mock_jwt.return_value = userinfo
+        output2 = profile(request)
+        # print("*****output 1234:",output2.content)
         self.assertEqual(output2.status_code, 200)
 
-    def test_get_id_based_tweets(self):
-        print("TC3: Get ID based Tweets Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output3 = get_id_based_tweets(request)
-        self.assertEqual(output3.status_code, 200)
-
-    def test_get_friends(self):
-        print("TC4: Get Friends Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output4 = get_friends(request)
-        self.assertEqual(output4.status_code, 200)
-
-    def test_get_followers(self):
-        print("TC5: Get Followers Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output5 = get_followers(request)
-        self.assertEqual(output5.status_code, 200)
-
-    def test_get_account_settings(self):
-        print("TC6: Get Account Settings Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output6 = get_account_settings(request)
-        self.assertEqual(output6.status_code, 200)
-
-    def test_get_privacy_policy(self):
-        print("TC7: Get Privacy Policy Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output7 = get_privacy_policy(request)
-        self.assertEqual(output7.status_code, 200)
-
-    def test_get_terms_of_service(self):
-        print("TC8: Get Privacy Policy Unit Test")
-        request = self.factory.get('/TwitterApi/TheSeekers/')
-
-        output8 = get_terms_of_service(request)
-        self.assertEqual(output8.status_code, 200)
+    # @patch('jwt.decode')
+    # def test_Home(self, mock_jwt):
+    #     print("TC3: Get Home Page:")
+    #     request = self.factory.get('/LMS/TheSeekers/')
+    #     request = self.add_session_to_request(request)
+    #     # print ('2222222---->', request.session)
+    #     # print(self.emp)
+    #     request.session['id_token'] = "abcd"
+    #     userinfo = {'http://Lms.seekers.com/app_metadata': {'role': 'Manager'},
+    #                 'http://Lms.seekers.com/user_metadata': {}, 'given_name': 'Big', 'name': 'Big Bob',
+    #                 'picture': 'https://lh4.googleusercontent.com/-rW-L8-Mdn6Y/AAAAAAAAAAI/AAAAAAAAGj0/dxNCMduKcmM/photo.jpg',
+    #                 'gender': 'female', 'locale': 'en-GB', 'updated_at': '2018-12-07T18:18:05.806Z',
+    #                 'email': 'bigbob@gmail.com', 'email_verified': True, 'iss': 'https://seekerslms.auth0.com/',
+    #                 'sub': 'google-oauth2|104087801749009073568', 'aud': 'LCTMUEpEUe9eV_0NWzAkUvkqF6cC19aT',
+    #                 'iat': 1544206686, 'exp': 1544242686}
+    #     mock_jwt.return_value = userinfo
+    #     output3 = Home(request)
+    #     # print("*****output 1234:",output2.content)
+    #     self.assertEqual(output3.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
 
-"""
